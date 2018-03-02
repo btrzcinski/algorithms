136e950e0f231b8f3ea20d3b285b354c318babbc
Barnett Trzcinski <btrzcinski@gmail.com>
2018-03-01 20:12:36 -0800

    Implemented selection sort

--- python/sorts/selection.py
+++ python/sorts/selection.py
@@ -1,2 +1,7 @@
 def sort(input):
-    pass
+    for i in range(len(input)):
+        min_element = input[i]
+        for j in range(i+1, len(input)):
+            if input[j] < input[min_element]:
+                min_element = j
+        input[i], input[min_element] = input[min_element], input[i]
