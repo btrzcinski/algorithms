caa74e8263f11a00d0c5695c914b9e4dc0277d53
Barnett Trzcinski <btrzcinski@gmail.com>
2018-03-01 20:17:01 -0800

    Implemented insertion sort

--- python/sorts/insertion.py
+++ python/sorts/insertion.py
@@ -1,2 +1,7 @@
 def sort(input):
-    pass
+    for i in range(1, len(input)):
+        while i > 0 and input[i] < input[i-1]:
+            input[i-1], input[i] = input[i], input[i-1]
+            i -= 1
+            
+        
